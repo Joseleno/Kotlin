4915fcdd0dc5c488c0a1ae3332e09e970c14111e
Joseleno <joselenomoreira@gmail.com>
2017-10-03 20:55:37 -0300

    TicTacToe avec Kotlin

---
+++ TicTacToe/app/src/androidTest/java/joseleno/santos/com/tictactoe/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package joseleno.santos.com.tictactoe
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("joseleno.santos.com.tictactoe", appContext.packageName)
+    }
+}
